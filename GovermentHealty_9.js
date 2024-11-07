1eaf19550cfad5d5cde7fd74ffe8892cf22372fe
rezalevii <154890651+rezalevii@users.noreply.github.com>
2024-11-07 21:05:19 +0700

    File Web  Pasir Kidul

---
+++ GovermentHealty_9.js
@@ -0,0 +1,1 @@
+var json_GovermentHealty_9 = {"type":"FeatureCollection","name":"GovermentHealty_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Nama":"Kantor Kelurahan Pasir Kidul"},"geometry":{"type":"Point","coordinates":[109.206410720170155,-7.419829035716902]}}]}
